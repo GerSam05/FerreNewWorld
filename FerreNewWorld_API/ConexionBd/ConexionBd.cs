0f1e736c084cfab7615f98aef69e5c2082c611e5
Gerardo Martinez <gerardosmartinezg@gmail.com>
2023-10-06 21:28:19 -0400

    Agregar archivos de proyecto.

---
+++ FerreNewWorld_API/ConexionBd/ConexionBd.cs
@@ -0,0 +1,18 @@
+﻿namespace FerreNewWorld_API.ConexionBd
+{
+    public class ConexionBd
+    {
+        private readonly string connectionString = string.Empty;
+
+        public ConexionBd()
+        {
+            var constructor = new ConfigurationBuilder().SetBasePath(
+                Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+            connectionString = constructor.GetSection("ConnectionStrings:conexionmaestra").Value;
+        }
+        public string CadenaSQL()
+        {
+            return connectionString;
+        }
+    }
+}
---
+++ FerreNewWorld_API/Modelos/Producto.cs
@@ -0,0 +1,12 @@
+﻿namespace FerreNewWorld_API.Modelos
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Descripcion { get; set; }
+        public string? Marca { get; set; }
+        public string Categoria { get; set;}
+        public decimal Precio { get; set;}
+    }
+}
